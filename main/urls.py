2afadac6cc546889494184bb1354e8facf62e1cf
vaflyaa <nikinikolaevich97@gmail.com>
2023-01-12 19:29:28 +0400

    modified:   main/__pycache__/urls.cpython-310.pyc 	modified:   main/__pycache__/views.cpython-310.pyc 	modified:   main/apps.py 	modified:   main/forms.py 	modified:   main/templates/layout/basic.html 	new file:   main/templates/main/delete_user.html 	new file:   main/templates/main/password_change.html 	new file:   main/templates/main/register_done.html 	new file:   main/templates/main/register_user.html 	modified:   main/urls.py 	modified:   main/views.py

--- main/urls.py
+++ main/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 
-from .views import index, other_page, profile, BBLoginView, BBLogout, ChangeUserInfoView
+from .views import (index, other_page, profile, 
+                    BBLoginView, BBLogout, ChangeUserInfoView, BBPasswordChangeView, RegisterDoneView, RegisterUserView, 
+                    DeleteUserView)
 
 app_name = 'main'
 urlpatterns = [
     path('<str:page>/', other_page, name = 'other'),
     path('', index, name='index'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name = 'register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name = 'register'),
     path('accounts/login/', BBLoginView.as_view(), name = 'login'),
     path('accounts/logout/', BBLogout.as_view(), name='logout'),
+    path('accounts/password/change/', BBPasswordChangeView.as_view(), name = 'password_change'),
+    path('accounts/profile/delete/', DeleteUserView.as_view(), name = 'profile_delete'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name = 'profile_change'),
     path('accounts/profile/', profile, name = 'profile'),
 
--- main/views.py
+++ main/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
+from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 
 from .models import AdvUser
-from .forms import ChangeUserInfoForm
+from .forms import ChangeUserInfoForm, RegisterUserForm
 
 def index(request):
     return render(request, 'main/index.html')
@@ -49,6 +52,44 @@ class ChangeUserInfoView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
         return super().setup(request, *args, **kwargs)
     
     # По этому user_id находим запись о user. Метод наследуется от SingleObjectMixin
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
+
+class BBPasswordChangeView(SuccessMessageMixin, LoginRequiredMixin, PasswordChangeView):
+    template_name = 'main/password_change.html'
+    success_url = reverse_lazy('main:profile')
+    success_message = 'Пароль пользователя успешно изменен'
+
+
+class RegisterUserView(CreateView):
+    model = AdvUser
+    template_name = 'main/register_user.html'
+    form_class = RegisterUserForm
+    success_url = reverse_lazy('main:register_done')
+
+
+class RegisterDoneView(TemplateView):
+    template_name = 'main/register_done.html'
+
+
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+    model = AdvUser
+    template_name = 'main/delete_user.html'
+    success_url = reverse_lazy('main:index')
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        # перед удалением, делаем выход и создаем всплывающие сообщение
+        logout(request)
+        messages.add_message(request, messages.SUCCESS, 'Пользователь удален')
+        return super().post(request, *args, **kwargs)
+
     def get_object(self, queryset=None):
         if not queryset:
             queryset = self.get_queryset()
