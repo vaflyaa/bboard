579249247cc00c88f9696fdc9a12e1e60a6ba847
vaflyaa <nikinikolaevich97@gmail.com>
2023-01-27 11:34:18 +0400

    Create new app for DRF

---
+++ api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from main.models import Bb
+
+class BbSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at')
+
+
+class BbDetailSerialzer(serializers.ModelSerializer):
+    class Meta:
+        model = Bb
+        fields = ('id', 'title', 'content', 'price', 'created_at', 'contacts', 'image')
---
+++ api/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.generics import RetrieveAPIView
+
+from main.models import Bb
+from .serializers import BbSerializer, BbDetailSerialzer
+
+@api_view(['GET'])
+def bbs(request):
+    if request.method == 'GET':
+        bbs = Bb.objects.filter(is_active=True) [:10]
+        serializer = BbSerializer(bbs, many=True)
+        return Response(serializer.data)
+
+
+class BbDetailView(RetrieveAPIView):
+    queryset = Bb.objects.filter(is_active=True)
+    serializer_class = BbDetailSerialzer
